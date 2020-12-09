d9cd9b7ac183b71e93d459687c4291bb1e75d9ab
belbanas <belbanas@gmail.com>
2020-12-09 14:55:31 +0100

    Fixed spaces added alert component

---
+++ app/views/ask.blade.php
@@ -0,0 +1,40 @@
+@extends('layout')
+
+@section('title', 'Ask a Question')
+
+@section('content')
+
+    @component('navbar')
+
+    @endcomponent
+
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-sm">
+                <h1 class="text-center">Ask Question</h1>
+            </div>
+        </div>
+        <div class="row align-items-center">
+            <div class="col-sm">
+            </div>
+            <div class="col-sm">
+                <form action="/registration" method="post">
+                    <div class="mb-3">
+                        <label for="email" class="form-label">Email</label>
+                        <input name="email" type="email" class="form-control" id="email" placeholder="Enter your email"
+                               required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleInputPassword1" class="form-label">Password</label>
+                        <input name="password" type="password" class="form-control" id="exampleInputPassword1"
+                               placeholder="Enter your password" required>
+                    </div>
+                    <button name="register" type="submit" class="btn btn-primary">Register</button>
+                </form>
+            </div>
+            <div class="col-sm">
+            </div>
+        </div>
+    </div>
+
+@endsection
