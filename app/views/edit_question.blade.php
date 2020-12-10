a3ee626255e6fe8ff2785f84409339ad1905628d
belbanas <belbanas@gmail.com>
2020-12-10 14:54:51 +0100

    Fix edit question

--- app/views/edit_question.blade.php
+++ app/views/edit_question.blade.php
@@ -12,34 +12,33 @@
 
         @if (isset($_SESSION['email']))
 
-        <table class="table table-striped">
-            <thead>
-            <tr>
-                <th scope="col">Id</th>
-                <th scope="col">Title</th>
-                <th scope="col">Message</th>
-                <th></th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr>
-                <form action='/edit_question_handler' method='POST'>
-                    <input type='hidden' name='id' value='{{$question->getId()}}'/>
-                    <th scope="row">{{ $question->getId() }}</th>
-                    <td>
-                        <label for="title"></label>
-                        <input type="text" value="{{ $question->getTitle() }}" name="title" id="title" required>
-                    </td>
-                    <td>
-                        <label for="message"></label>
-                        <input type="text" value="{{ $question->getMessage() }}" name="message" id="message" required>
-                    </td>
-                    <td><input type="submit" value="Send Request"></td>
-                </form>
-            </tr>
-
-            </tbody>
-        </table>
+            <div class="row">
+                <div class="col-sm">
+                    <h1 class="text-center">Edit Question</h1>
+                </div>
+            </div>
+            <div class="row align-items-center">
+                <div class="col-sm">
+                </div>
+                <div class="col-sm">
+                    <form action="/edit_question_handler" method="post">
+                        <input type='hidden' name='id' value='{{$question->getId()}}'/>
+                        <div class="mb-3">
+                            <label for="title" class="form-label">Title</label>
+                            <input name="title" type="text" class="form-control" id="title"
+                                   value="{{ $question->getTitle() }}">
+                        </div>
+                        <label for="question" class="form-label">Question</label>
+                        <div class="input-group mb-3">
+                            <textarea id="message" class="form-control" aria-label="With textarea" name="message"
+                                      rows="6">{{ $question->getMessage() }}</textarea>
+                        </div>
+                        <button name="edit" type="submit" class="btn btn-primary">Send Question</button>
+                    </form>
+                </div>
+                <div class="col-sm">
+                </div>
+            </div>
 
         @else
 
