28c7090c9d7dcc7fc6bd9c86181ff15ab8b42856
belbanas <belbanas@gmail.com>
2020-12-10 13:51:07 +0100

    Fix users.blade.php layout

--- app/views/users.blade.php
+++ app/views/users.blade.php
@@ -1,35 +1,37 @@
-@extends('navbar')
+@extends('layout')
 
 @section('title', 'Question')
 
 @section('content')
 
+    @component('navbar')
 
+    @endcomponent
 
-<div class="container mt-5">
-    <table class="table table-striped">
-        <thead>
-        <tr>
-            <th scope="col">User Id</th>
-            <th scope="col">User Email</th>
-            <th scope="col">Registration Date</th>
-            <th scope="col">Count of asked questions</th>
-            <th scope="col">Count of answers</th>
-        </tr>
-        </thead>
-        <tbody>
-        @foreach ($users as $user)
+    <div class="container mt-5">
+        <table class="table table-striped">
+            <thead>
             <tr>
-                <td>{{$user->getId()}}</td>
-                <td>{{$user->getEmail()}}</td>
-                <td>{{$user->getRegistrationTime()}}</td>
-                <td>{{$user->getNumberOfQuestions()}}</td>
-                <td>{{$user->getNumberOfAnswers()}}</td>
+                <th scope="col">User Id</th>
+                <th scope="col">User Email</th>
+                <th scope="col">Registration Date</th>
+                <th scope="col">Count of asked questions</th>
+                <th scope="col">Count of answers</th>
             </tr>
-        @endforeach
-        </tbody>
-    </table>
-    <a href="/"><button type="button" class="btn btn-primary">Back</button></a>
-</div>
+            </thead>
+            <tbody>
+            @foreach ($users as $user)
+                <tr>
+                    <td>{{$user->getId()}}</td>
+                    <td>{{$user->getEmail()}}</td>
+                    <td>{{$user->getRegistrationTime()}}</td>
+                    <td>{{$user->getNumberOfQuestions()}}</td>
+                    <td>{{$user->getNumberOfAnswers()}}</td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+        <a href="/"><button type="button" class="btn btn-primary">Back</button></a>
+    </div>
 
 @endsection
